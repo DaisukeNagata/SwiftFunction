51e05f63c95509038c38b072092fca7f8487a86e
大祐永田 <dbank0208@gmail.com>
2021-05-31 06:47:01 +0900

    update: SortedFunction

--- Sources/SwiftFunction/SortedFunction.swift
+++ Sources/SwiftFunction/SortedFunction.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-final class SortedFunction: NSObject {
+public class SortedFunction: NSObject {
+
     public func dicSorted(dic: Dictionary<String, Int>) {
         let thisissorted =  dic.sorted { (l, r) -> Bool in
             return l.value < r.value ? false : true
         }
-
         print(thisissorted)
     }
 }
