0cf6b3d02f2ab33ec1019e7daa99ee1029809b24
大祐永田 <dbank0208@gmail.com>
2021-06-01 07:41:31 +0900

    update: GenericsFunction

--- Sources/SwiftFunction/GenericsFunction.swift
+++ Sources/SwiftFunction/GenericsFunction.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public class GenericsFunction: NSObject {
 
-    func bigger<T:Comparable>(_ val1: T, val2: T,val3: T) -> T {
+    public func bigger<T:Comparable>(_ val1: T, val2: T,val3: T) -> T {
         return max(val1, val2,val3)
     }
 
-    func smaller<T:Comparable>(_ val1: T, val2: T,val3: T) -> T {
+    public func smaller<T:Comparable>(_ val1: T, val2: T,val3: T) -> T {
         return min(val1, val2,val3)
     }
 }
