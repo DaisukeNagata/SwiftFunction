619bb57d4b9c1803c35774ad19df7442051d4812
大祐永田 <dbank0208@gmail.com>
2021-06-09 05:47:35 +0900

    add: comment

--- Tests/SwiftFunctionTests/SwiftFunctionTests.swift
+++ Tests/SwiftFunctionTests/SwiftFunctionTests.swift
@@ -28,18 +28,23 @@
             // XCTAssertEqual(SwiftFunction.text, "Hello, World!")
 
             // ClosureFunction
+            print("--ClosureFunction--")
             print(closureFunction.numberComponent(value: 777) ?? 0)
+
             // EnumFunction
+            print("--EnumFunction--")
             enumFunction.abcEnum(e: EnumFunction.ABC.a)
             print(EnumFunction.Count(rawValue: 0)!)
             print(enumFunction.checkAttr(attr: EnumFunction.Person.Age(age: 20)))
             print(enumFunction.checkAttr(attr: EnumFunction.Person.Name(name: "D")))
 
             // FirstIndexFunction
+            print("--FirstIndexFunction--")
             firstIndexFunction.firstIndexFunction()
             firstIndexFunction.checkIndexFunction(numArray: [60, 70, 90, nil, 90, 0, 80, 80, 0, 80])
 
             // ForFunction
+            print("--ForFunction--")
             forFunction.forAction(start: 0, end: 10)
             forFunction.forEatchAction(index: [1,2,3])
             forFunction.forInstanceAction(instance: [1,2,3])
@@ -47,6 +52,7 @@
             forFunction.forWhereInt(index: [1,2,3], fix: 3)
 
             // GenericsFunction
+            print("--GenericsFunction--")
             let a = 10
             let b = 15
             let c = 20
@@ -55,6 +61,7 @@
             print(genericsFunction.smaller(a, val2: b, val3: c))
 
             // HashFunction
+            print("--HashFunction--")
             let h = HashFunction.init(sectionID: "123", itemID: "345E")
             var hasher = Hasher()
             hasher.combine(h.sectionIDHash)
@@ -66,11 +73,13 @@
             print(hash, "hash")
 
             // JsonFunction
+            print("--JsonFunction--")
             /// ①初期オブジェクト生成
             let originalObject = JsonFunction.Employee(code: "001", name: "山田", age: 45, absence: false)
             jsonFunction.createJson(originalObject: originalObject)
     
             // MapFunction
+            print("--MapFunction--")
             mapFunction.totalFunction()
             let items = ["A", "B", "C", "D", "A", "A"]
             print(items.indexes(of: "A"))
@@ -89,19 +98,26 @@
             print(mathematicsFunction.coordinate(cg: 100), "coordinate")
 
             // MemoryUsage
+            print("--MemoryUsage--")
             print(memoryUsage.checkPointer())
             print(memoryUsage.reportMemory())
 
             // StringSplitFunction
+            print("--StringSplitFunction--")
             stringSplitFunction.stringSplit(str: "Hell12o, p3laygrou4.5nd", rangeSt: "[0-9-.]")
 
             // SwitchFunction
+            print("--SwitchFunction--")
             switchFunction.switchWhere(token: "123")
             switchFunction.switchWhere(token: "")
             switchFunction.switchWhere(token: nil)
+
             // SortedFunction
+            print("--SortedFunction--")
             sortedFunction.dicSorted(dic: ["111": 111, "222": 222, "333": 333, "444": 444, "555": 555])
+
             // StringFunction
+            print("--StringFunction--")
             let attributedStr = stringFunction.stringAttributes(title: "123", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), size: 17)
             let hyperlinkAtribute = stringFunction.hyperlinkAtribute(termsTitle: "Google",
                                                                      lineSpacing: 5,
@@ -109,14 +125,18 @@
                                                                      title: "Google")
             print(attributedStr.string)
             print(hyperlinkAtribute.string)
+
             // OrderedSetFunction
+            print("--OrderedSetFunction--")
             orderedSetFunction.orderedSet(searchWords: ["a","b","c","d","d","c","e","e"])
             
             // ProtocolFunction
+            print("--ProtocolFunction--")
             protocolFunctionB.delegate = protocolFunctionA
             protocolFunctionB.delegate?.protocolFunction(type: "type")
 
             // WhileFunction
+            print("--WhileFunction--")
             print(whileFunction.whileCount(value: 10))
             print(whileFunction.repeatCount(value: 10))
             print(whileFunction.whileJudge(value: 10))
