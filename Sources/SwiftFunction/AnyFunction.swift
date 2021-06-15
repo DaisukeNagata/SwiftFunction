34dda4321378f219985fc5c1407c51beb4d30685
大祐永田 <dbank0208@gmail.com>
2021-06-15 09:15:33 +0900

    update: anyIndexDataCheck function by AnyFunction

--- Sources/SwiftFunction/AnyFunction.swift
+++ Sources/SwiftFunction/AnyFunction.swift
@@ -17,26 +17,30 @@ public struct Abc3 {
     public var asd = 3
 }
 
+
 public class AnyFunction {
 
     public func anyIndexDataCheck() {
         var sampleAny: [Any] = []
         var sampleAny2: [Any] = []
-        var sampleAny3: [Any] = []
 
         sampleAny = [Abc1(), Abc3(), Abc3(), Abc1(), Abc2(), Abc3()]
         for value in sampleAny {
             if cast(value: value, type: Abc1.self) != nil || cast(value: value, type: Abc3.self) != nil {
                 sampleAny2.append(value)
             } else {
-                sampleAny3.append(value)
+                if let sample = value as? Abc1 {
+                    print(sample.asd, "🟢anyFunctionAbc1")
+                }
+                if let sample = value as? Abc2 {
+                    print(sample.asd, "🟢anyFunctionAbc2")
+                }
+                if let sample = value as? Abc3 {
+                    print(sample.asd, "🟢anyFunctionAbc3")
+                }
             }
         }
         print(sampleAny2, "🟢anyFunction")
-        guard let sample = sampleAny3 as? [Abc2] else { return }
-        sample.forEach { s in
-            print(s.asd, "🟢anyFunction")
-        }
     }
 
     func cast<A>(value: Any?, type: A.Type) -> A? { return value as? A }
