f2953015e8653b0cd3d1c99b8933595a1e4d723e
大祐永田 <dbank0208@gmail.com>
2021-06-21 06:41:47 +0900

    add: extension date function

--- Sources/SwiftFunction/DateFunction.swift
+++ Sources/SwiftFunction/DateFunction.swift
@@ -46,3 +46,101 @@ public class DateFunction {
         }
     }
 }
+
+extension TimeZone {
+
+    static let japan = TimeZone(identifier: "Asia/Tokyo")!
+}
+
+extension Locale {
+    
+    static let japan = Locale(identifier: "ja_JP")
+}
+
+extension Date {
+
+    init(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) {
+            self.init(
+                timeIntervalSince1970: Date().fixed(
+                    year:   year,
+                    month:  month,
+                    day:    day,
+                    hour:   hour,
+                    minute: minute,
+                    second: second
+                ).timeIntervalSince1970
+            )
+        }
+
+    var weekName: String {
+        let index = calendar.component(.weekday, from: self) - 1 // index値を 1〜7 から 0〜6 にしている
+        return ["日", "月", "火", "水", "木", "金", "土"][index]
+    }
+
+    var year: Int {
+        return calendar.component(.year, from: self)
+    }
+
+    var month: Int {
+        return calendar.component(.month, from: self)
+    }
+
+    var day: Int {
+        return calendar.component(.day, from: self)
+    }
+
+    var hour: Int {
+        return calendar.component(.hour, from: self)
+    }
+
+    var minute: Int {
+        return calendar.component(.minute, from: self)
+    }
+
+    var second: Int {
+        return calendar.component(.second, from: self)
+    }
+
+    var calendar: Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .current
+        calendar.locale   = .current
+        return calendar
+    }
+
+    var oclock: Date {
+        return fixed(minute: 0, second: 0)
+    }
+
+    var zeroclock: Date {
+        return fixed(hour: 0, minute: 0, second: 0)
+    }
+
+    func fixed(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        let calendar = self.calendar
+
+        var comp = DateComponents()
+        comp.year   = year   ?? calendar.component(.year,   from: self)
+        comp.month  = month  ?? calendar.component(.month,  from: self)
+        comp.day    = day    ?? calendar.component(.day,    from: self)
+        comp.hour   = hour   ?? calendar.component(.hour,   from: self)
+        comp.minute = minute ?? calendar.component(.minute, from: self)
+        comp.second = second ?? calendar.component(.second, from: self)
+        
+        return calendar.date(from: comp)!
+    }
+
+    func added(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        let calendar = self.calendar
+
+        var comp = DateComponents()
+        comp.year   = (year   ?? 0) + calendar.component(.year,   from: self)
+        comp.month  = (month  ?? 0) + calendar.component(.month,  from: self)
+        comp.day    = (day    ?? 0) + calendar.component(.day,    from: self)
+        comp.hour   = (hour   ?? 0) + calendar.component(.hour,   from: self)
+        comp.minute = (minute ?? 0) + calendar.component(.minute, from: self)
+        comp.second = (second ?? 0) + calendar.component(.second, from: self)
+
+        return calendar.date(from: comp)!
+    }
+}
