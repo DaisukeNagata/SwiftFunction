1a5c510d64cbd3f4b0852fd3c93f0565e8682a0a
大祐永田 <dbank0208@gmail.com>
2021-07-14 07:48:04 +0900

    add: TextDesgin

---
+++ Sources/SwiftFunction/SwiftUI/TextDesgin.swift
@@ -0,0 +1,22 @@
+//
+//  TextDesgin.swift
+//  
+//
+//  Created by 永田大祐 on 2021/07/14.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct TextView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+@available(iOS 14.0, *)
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
