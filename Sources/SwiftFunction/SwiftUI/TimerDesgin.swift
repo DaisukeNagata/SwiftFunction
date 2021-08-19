a1219a73a51bb0da89fbc175f1232ad3a367ed6d
大祐永田 <dbank0208@gmail.com>
2021-08-19 22:19:06 +0900

    add: TimerDesginView

---
+++ Sources/SwiftFunction/SwiftUI/TimerDesgin.swift
@@ -0,0 +1,31 @@
+//
+//  TimerDesgin.swift
+//  TimerDesgin
+//
+//  Created by 永田大祐 on 2021/08/19.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct TimerDesginView: View {
+    @State var timeRemaining = 10
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        Text("\(timeRemaining)")
+            .onReceive(timer) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
+            }
+    }
+}
+
+@available(iOS 14.0, *)
+struct TimerDesginView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerDesginView()
+    }
+}
+
