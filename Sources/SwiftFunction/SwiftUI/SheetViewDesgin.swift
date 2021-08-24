bef84c4e44025006a342fc1ad9ee3da4a7075ce1
大祐永田 <dbank0208@gmail.com>
2021-08-25 07:50:28 +0900

    add: SheetView

---
+++ Sources/SwiftFunction/SwiftUI/SheetViewDesgin.swift
@@ -0,0 +1,36 @@
+//
+//  SheetView.swift
+//  SheetView
+//
+//  Created by 永田大祐 on 2021/08/25.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct SheetView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var body: some View {
+            NavigationView { // Mandatory for Title and Button
+                Form {
+                    Text("1")
+                    Text("2")
+                }
+                .navigationBarTitle(Text("Title"))
+                .navigationBarItems(
+                    trailing: Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Some thing")
+                })
+            }.environment(\.horizontalSizeClass, .compact)
+    }
+}
+
+@available(iOS 14.0, *)
+struct SheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetView()
+    }
+}
