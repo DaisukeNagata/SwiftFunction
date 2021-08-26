b112c2397fcaa23fc910280b7243fa62b443aad4
大祐永田 <dbank0208@gmail.com>
2021-08-26 22:52:45 +0900

    update: SliderDesginView

--- Sources/SwiftFunction/SwiftUI/SliderDesgin.swift
+++ Sources/SwiftFunction/SwiftUI/SliderDesgin.swift
@@ -12,19 +12,22 @@ struct SliderDesginView: View {
     @State var littleValue: Float = 0.5
     @State var bigValue: Float = 50
     @State var isEditing: Bool = false
-
     var body: some View {
-        VStack {
-            Slider(value: $littleValue)
-            Text("Float: \(littleValue)")
-
-            Slider(value: $bigValue, in: 0...100, step: 5) { editing in
-                isEditing = editing
-            }
-            .accentColor(.red)
-            Text("Int: \(bigValue)")
-            if isEditing {
-                Text("Editing!")
+        ZStack {
+            VStack {
+                Slider(value: $littleValue)
+                Text("Float: \(littleValue)")
+                
+                Slider(value: $bigValue, in: 0...100, step: 5) { editing in
+                    isEditing = editing
+                }
+                .accentColor(.red)
+                Text("Int: \(bigValue)")
+                if isEditing {
+                    Text("Editing!")
+                } else {
+                    Text(" ")
+                }
             }
         }
     }
