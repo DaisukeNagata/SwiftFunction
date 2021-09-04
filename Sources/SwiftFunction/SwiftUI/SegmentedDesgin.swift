6609ebf5408ecd63812f20e8852b627eef81108a
大祐永田 <dbank0208@gmail.com>
2021-09-04 15:04:56 +0900

    update: SegmentedView

--- Sources/SwiftFunction/SwiftUI/SegmentedDesgin.swift
+++ Sources/SwiftFunction/SwiftUI/SegmentedDesgin.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 struct SegmentedView: View {
-    
+    @State private var edgeFront = Edge.leading
+    @State private var edgeBack = Edge.trailing
     @State private var selectedIndex = 0
     
     var body: some View {
@@ -21,16 +22,44 @@ struct SegmentedView: View {
                     .tag(1)
                 Text("\(2)")
                     .tag(2)
+                Text("\(3)")
+                    .tag(3)
             }
             .pickerStyle(SegmentedPickerStyle())
             
             switch selectedIndex {
             case 0:
                 viewA
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)
+                        ))
+                    .animation(.default)
             case 1:
                 viewB
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: edgeFront),
+                            removal: .move(edge: edgeBack)
+                        ))
+                    .animation(.easeInOut)
             case 2:
                 viewC
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: edgeFront),
+                            removal: .move(edge: edgeBack)
+                        ))
+                    .animation(.easeInOut)
+            case 3:
+                viewD
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: .leading),
+                            removal: .move(edge: .trailing)
+                        ))
+                    .animation(.default)
             default:
                 viewA
             }
@@ -41,6 +70,10 @@ struct SegmentedView: View {
         ZStack {
             Color(.blue).edgesIgnoringSafeArea(.all)
             Text("A").foregroundColor(.black)
+                .onAppear{
+                    edgeFront = Edge.leading
+                    edgeBack = Edge.trailing
+                }
         }
     }
     private var viewB: some View {
@@ -53,6 +86,17 @@ struct SegmentedView: View {
         ZStack {
             Color(.green).edgesIgnoringSafeArea(.all)
             Text("C").foregroundColor(.black)
+        }
+    }
+    
+    private var viewD: some View {
+        ZStack {
+            Color(.orange).edgesIgnoringSafeArea(.all)
+            Text("D").foregroundColor(.black)
+                .onAppear{
+                    edgeFront = Edge.trailing
+                    edgeBack = Edge.leading
+                }
         }
     }
 }
