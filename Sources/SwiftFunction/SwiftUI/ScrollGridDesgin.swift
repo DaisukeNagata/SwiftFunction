d2c6de4e08030bf690d0396f46bc20a14a214e71
大祐永田 <dbank0208@gmail.com>
2021-09-15 07:04:34 +0900

    add: ScrollGridDesgin

---
+++ Sources/SwiftFunction/SwiftUI/ScrollGridDesgin.swift
@@ -0,0 +1,32 @@
+//
+//  ScrollGridView.swift
+//
+//
+//  Created by 永田大祐 on 2021/09/15.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct ScrollGridView: View {
+    var body: some View {
+        ScrollView {
+            ForEach((0...4), id: \.self) { row in
+                HStack {
+                    ForEach((1...4), id: \.self) { column in
+                        Text("\(row*4+column)")
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .padding(.all, 30)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+struct ScrollGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollGridView()
+    }
+}
