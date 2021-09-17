de859bb3c78ec905014ec05655371591a042c18a
大祐永田 <dbank0208@gmail.com>
2021-09-18 08:20:43 +0900

    update: ScrollGridView

--- Sources/SwiftFunction/SwiftUI/ScrollGridDesgin.swift
+++ Sources/SwiftFunction/SwiftUI/ScrollGridDesgin.swift
@@ -11,10 +11,10 @@ import SwiftUI
 struct ScrollGridView: View {
     var body: some View {
         ScrollView {
-            ForEach((0...4), id: \.self) { row in
+            ForEach((0...9), id: \.self) { row in
                 HStack {
-                    ForEach((0...4), id: \.self) { column in
-                        Text("\(row*4+column)")
+                    ForEach((1...10), id: \.self) { column in
+                        Text("\(row*10+column)")
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
                             .padding(.trailing, 5)
                             .padding(.top, 30)
