419b44f8318700ef873cd2a9794ce8acee927d2d
大祐永田 <dbank0208@gmail.com>
2021-10-01 02:18:52 +0900

    add: ButtonStyleView

---
+++ Sources/SwiftFunction/SwiftUI/ButtonStyleDegin.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonStyleView.swift
+//  
+//
+//  Created by 永田大祐 on 2021/09/30.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct ButtonStyleView: View {
+    var body: some View {
+        VStack {
+
+            Button(action: {}) {
+                Text("Buy me a coffee")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+struct ButtonStyleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStyleView()
+    }
+}
